00354d77abaaa54baf9f260836e7db1e7c805300
martin <mpventer@sun.ac.za>
2019-11-20 13:49:39 +0200

    There is now a class that creates and runs a dyna simulation

--- Utils.py
+++ Utils.py
@@ -10,6 +10,11 @@ Additional utilitis required for processing of dyna results
 """
 import numpy as np
 import networkx as nx
+import os
+import subprocess
+
+from string import Template
+from datetime import date, datetime
 
 from sklearn.base import BaseEstimator, TransformerMixin
 
@@ -227,4 +232,213 @@ def extract_observed_surface(d3plot, time_step=0, eps=0.0001) -> list:
     # return the nodes meeting this criteria
     return node_ids_observed
 
-# TODO
+
+class PuckSim:
+    """
+    Generate all files required to run a new puck compression simulation dyna
+    simulation
+    """
+
+    def __init__(
+            self, sim_name, cfile_template_name, keyword_main_template_name,
+            template_dir=None, sim_dir=None):
+        """
+        initialise the file and direcory names for a puck simulation
+        Parameters
+        ----------
+        sim_name: str
+        cfile_template_name: str
+        keyword_main_template_name: str
+        template_dir: str
+        sim_dir: str
+        """
+        self.sim_name = sim_name
+        self.cfile_template_name = cfile_template_name
+        self.keyword_main_template_name = keyword_main_template_name
+        self.path_root = os.getcwd()
+
+        # set default paths
+        if template_dir is None:
+            self.template_dir = os.path.join(self.path_root, "templates")
+        else:
+            self.template_dir = template_dir
+
+        self.cfile_template = os.path.join(
+            self.template_dir, self.cfile_template_name)
+        self.keyword_main_template = os.path.join(
+            self.template_dir, self.keyword_main_template_name)
+
+        if sim_dir is None:
+            self.sim_dir = os.path.join(self.path_root, "TMP")
+        else:
+            self.sim_dir = sim_dir
+
+        self.sim_wd = os.path.join(self.sim_dir, self.sim_name)
+
+        # set remaining paths and names
+        self.cfile_name = '{}.msg.cfile'.format(self.sim_name)
+        self.keyword_geom_name = "{}_geom.k".format(self.sim_name)
+        self.keyword_main_name = "{}_main.k".format(self.sim_name)
+        self.job_number = 1
+
+        self.cfile = os.path.join(self.sim_wd, self.cfile_name)
+        self.keyword_geom = os.path.join(self.sim_wd, self.keyword_geom_name)
+        self.keyword_main = os.path.join(self.sim_wd, self.keyword_main_name)
+
+        # additional parameters for model generation
+        self.radius = None
+        self.length = None
+        self.num_elements = None
+        self.num = None
+        self.date_created = None
+        self.time_created = None
+        self.title = None
+        self.date_time = None
+
+        # software paths
+        self.lspp_path = "/home/martin/.local/bin/lspp47"
+        self.dyna_path = "/home/martin/Programs/LSTC/LSDYNA/"
+        self.dyna_s_path = os.path.join(self.dyna_path,
+                                        "ls-dyna_smp_s_r1010_x64_suse11_pgi165")
+        self.dyna_d_path = os.path.join(self.dyna_path,
+                                        "ls-dyna_smp_d_r1010_x64_suse11_pgi165")
+
+        self.sim_output = None
+        self.sim_error = None
+
+    def setup_simulation(self, radius, length, num_elements) -> None:
+        """
+        update the mesh
+        Parameters
+        ----------
+        radius: float
+        length: float
+        num_elements: int
+
+        Returns
+        -------
+            None
+        """
+        self.radius = radius
+        self.length = length
+        self.num_elements = num_elements
+        cylinder_ratio = (self.radius * 2 * np.pi) / self.length
+        self.num = int(self.num_elements / cylinder_ratio)
+
+        self.date_created = date.today()
+        self.time_created = datetime.now().strftime("%H:%M:%S")
+        self.title = "{} [{}]".format(self.sim_name, self.date_created)
+        self.date_time = "{} ({})".format(self.date_created, self.time_created)
+
+        # setup all components of the simulation
+        self._make_cfile()
+        self._build_geom_keyword()
+        self._update_main_keyword()
+
+    def _make_cfile(self) -> None:
+        """
+        reads the cfile macro and updates the geometry parameters, saves the
+        updated cfile in the target directory
+        Parameters
+        ----------
+
+        Returns
+        -------
+            None
+        """
+        match = {
+            "working_directory": self.sim_wd,
+            "keyword_file_name": self.keyword_geom
+        }
+        with open(self.cfile_template, 'r') as f:
+            data = f.read()
+        # f.closed
+
+        data = self.replace(data, match)
+
+        with open(self.cfile, "w") as text_file:
+            print(data, file=text_file)
+
+    @staticmethod
+    def replace(text, dic) -> str:
+        """
+        replaces text in a given file.
+        Parameters
+        ----------
+        text
+        dic
+
+        Returns
+        -------
+
+        """
+        s = Template(text)
+        return s.safe_substitute(**dic)
+
+    def _build_geom_keyword(self) -> None:
+        """
+        Runs the lsprepost macro and creates a new geometry keyword file
+        Returns
+        -------
+            None
+        """
+        bash_command = "{} c={} -nographics".format(
+            self.lspp_path,
+            self.cfile)
+
+        process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+
+    def _update_main_keyword(self):
+        """
+        Make updates to the main keyword file so that it includes the updated
+        Name and links to other files
+        Returns
+        -------
+
+        """
+        match = {
+            "date_time": self.date_time,
+            "title": self.title,
+            "include_file": self.keyword_geom
+        }
+        with open(self.keyword_main_template, 'r') as f:
+            data = f.read()
+        # f.closed
+
+        data = self.replace(data, match)
+
+        with open(self.keyword_main, "w") as text_file:
+            print(data, file=text_file)
+
+    def run_simulation(self) -> str:
+        print("start sim")
+        d = dict(os.environ)
+        d["LSTC_LICENSE"] = 'network'
+        d["LSTC_LICENSE_SERVER"] = '31010@license.sun.ac.za'
+
+        bash_command = [
+            self.dyna_s_path,
+            "i={}".format(self.keyword_main),
+            "ncpu=8",
+            "memory=1000m",
+            "jobid={}_job_{}".format(self.sim_name, self.job_number)
+        ]
+
+        process = subprocess.Popen(bash_command,
+                                   cwd=self.sim_wd,
+                                   stdout=subprocess.PIPE, env=d)
+        output, error = process.communicate()
+        self.sim_output = output.decode('ascii')
+        self.sim_error = error
+        print("end sim")
+        self.job_number += 1
+
+
+# self.job_name = "{}_job_{}".format(self.job_number)
+
+# TODO create a new class for generating puck geometries
+# TODO method update cfile
+# TODO method run cfile to creat gemetry.k
+# TODO method update main.k
+# TODO method run main.k in given wd
